5c708346895819108f3a6e69d95cc536eda81aa3
Satyamjha24 <pananrocks18@gmail.com>
2023-07-17 14:44:23 +0530

    backend

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const { connection } = require('./db')
+const cors=require('cors')
+require('dotenv').config()
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.status(200).send("Welcome To Carbon_Board")
+})
+
+
+
+
+app.listen(process.env.port, async()=>{
+      try{
+        await connection
+        console.log('Connected to DB');
+      }catch(err){
+        console.log(err);
+        console.log("Something went wrong");
+      }
+       console.log(`Server is running on port ${process.env.port}`);
+})
